cf08973788291f0312989ae068cb868b8fc6050a
michael@zedeler.dk <michael@zedeler.dk>
2023-12-29 00:11:57 +0100

    Misc. cleanup.

--- index.test.tsx
+++ index.test.tsx
@@ -1,13 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import * as mocha from 'mocha'
+import { describe, it } from 'mocha'
 import { use, expect } from 'chai'
 import chaiDom from 'chai-dom'
 
 use(chaiDom)
 
-console.log('loaded')
-mocha.describe('hello', () => {
-  mocha.it('works', () => {
+describe('hello', () => {
+  it('works', () => {
     const MyComponent = () => <h1>hello</h1>
 
     render(<MyComponent />)

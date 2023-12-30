109fcae19bab6177a69f7f120e6dfbc98ecf243d
michael@zedeler.dk <michael@zedeler.dk>
2023-12-30 01:25:35 +0100

    Now with README and some more cleanup.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-generator-components
 Generator based rendering for React components
+
+## Try the playground
+```
+npm i
+npm run develop -- --open
+```
--- src/playground/components/Playground.tsx
+++ src/playground/components/Playground.tsx
@@ -8,14 +8,18 @@ export const Playground = () => (
     <details>
       <summary>A timer</summary>
       <Timer />
+      <p>This is just a basic timer (note that it keeps running even when hidden).</p>
     </details>
     <details>
       <summary>An input</summary>
       <Input />
+      <p>Try typing "hello". (There is no way to reset except by reloading.)</p>
     </details>
     <details>
       <summary>A stream based input</summary>
       <StreamBasedInput />
+      <p>This input stays enabled until it contains "hello" like the other one, but
+        it uses a stream internally (try looking at the source).</p>
     </details>
   </>
 )
